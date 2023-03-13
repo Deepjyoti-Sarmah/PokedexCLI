a794f3d33270f3114bb207668804f24c690234af
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-03-13 19:21:31 +0530

    made a repl for cli pokedex

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Print(" >")
-
-	scanner.Scan()
-	test := scanner.Text()
-
-	fmt.Println("echoing: ", test)
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -4,16 +4,59 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Printf(" >")
+	for {
+		fmt.Printf(" >")
 
-	scanner.Scan()
-	text := scanner.Text()
+		scanner.Scan()
+		text := scanner.Text()
 
-	fmt.Println("echoing:", text)
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
 
+		commandName := cleaned[0]
+
+		availableCommands := getCommands()
+
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("Invalid Command")
+			continue
+		}
+		command.callback()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Turns off the Pokedex",
+			callback:    callbackExit,
+		},
+	}
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
 }
